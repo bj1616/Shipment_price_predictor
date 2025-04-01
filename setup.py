cec015158a1539ece17ec680e5078658cb742a74
bj1616 <jha02458@gmail.com>
2025-04-01 21:05:24 +0530

    new commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="shipment",
+    author="bhaskar",
+    version='0.0.1',
+    author_email="k46051799@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
+
